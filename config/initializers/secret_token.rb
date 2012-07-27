6b22dca9867f5f88af6706e35ab653b542a37221
Leonid Batizhevsky <the.leonko@gmail.com>
2012-07-27 09:48:39 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flatsoft::Application.config.secret_token = 'b53461776a7493259c112f740f92081fac62d3c78d7d4c3caced7982ebaacaedb2e33d3949e0040f6f773b842849d50e531d1d2ad99d892d8d4f8883cb3f9df6'
